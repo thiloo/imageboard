b72445895bc1a27960f87498e709ef9c569e5401
Thilo Roth <thilo@thilo.io>
2016-10-27 17:33:39 +0200

    upload, single view, description implemented. Comments still to do

--- main.js
+++ main.js
@@ -1,16 +1,71 @@
 const express = require('express'),
     app = express(),
     client = require('./modules/getClient'),
-    query = require('./content/queries.json');
+    query = require('./content/queries.json'),
+    bodyParser = require('body-parser'),
+    multer = require('multer');
+
+var diskStorage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, __dirname + '/public/uploads');
+    },
+    filename: function(req, file, callback) {
+        callback(null, Date.now() + '_' + Math.floor(Math.random() * 99999999) + '_' + file.originalname);
+    }
+});
+var maxSize = 2097152;
+var uploader = multer({
+    storage: diskStorage,
+    limits: {
+        filesize: maxSize
+    }
+});
 
 // upon starting with a clean database run uncomment the following line below;
 // var dbcontent = require('./modules/populateDB.js');
+
+app.use(bodyParser.json());
 app.get('/images', function(req, res){
     client.client(query.initialImageLoad)
         .then(function(images){
             res.json(images);
         });
 });
+app.get('/image/:imageName', function(req, res){
+    var image = req.url.split('/').pop();
+    client.client(query.getImage, [image])
+        .then(function(response){
+            res.json(response);
+        });
+});
+app.post('/upload', uploader.single('file'), function(req, res){
+    if (req.file) {
+        res.json({
+            success: true,
+            file: '/uploads/' + req.file.filename
+        });
+    } else {
+        res.json({
+            success: false
+        });
+    }
+});
+app.post('/store', function(req, res){
+    client.client(query.addImage, [req.body.url.file, req.body.title, req.body.description])
+        .then(function(id){
+            res.json({
+                succes: true,
+                id: id.rows[0].id
+            });
+        })
+        .catch(function(error){
+            console.log(error);
+            res.json({
+                success: false,
+                reason: error
+            });
+        });
+});
+
 app.use(express.static('public'));
-app.use('/uploads', express.static('/uploads'));
 app.listen(8080);
--- public/js/client.js
+++ public/js/client.js
@@ -1,25 +1,77 @@
-(function(){
+(function() {
+
+    window.site = {
+        routes: {},
+        views: {},
+        models: {}
+    };
+
     var Router = Backbone.Router.extend({
         routes: {
-            'images': 'images'
+            '': 'innitial',
+            'image/:id': 'image',
+            'upload': 'upload'
         },
-        images: function() {
-            console.log('yoyo');
+        innitial: function() {
             new UsersView({
-                model: usersModel,
+                model: new LoadModel,
+                el: '#main'
+            });
+        },
+        image: function(id) {
+            new ImageView({
+                model: new ImageModel({
+                    id: id
+                }),
+                el: '#main'
+            });
+        },
+        upload: function() {
+            new UploadView({
+                model: new UploadModel,
                 el: '#main'
             });
         }
     });
 
-    var UsersModel = Backbone.Model.extend({
+    var LoadModel = Backbone.Model.extend({
         initialize: function() {
             this.fetch();
         },
         url: '/images',
     });
 
-    var usersModel = new UsersModel;
+    var ImageModel = Backbone.Model.extend({
+        baseUrl: '/image',
+        url: function() {return this.baseUrl + '/' + this.id;},
+        initialize: function() {
+            this.fetch('id');
+        }
+    });
+
+    var UploadModel = Backbone.Model.extend({
+        upload: function(data) {
+            var load = this;
+            $.ajax({
+                url: '/upload',
+                method: 'POST',
+                data: data.file,
+                processData: false,
+                contentType: false,
+                success: function(path) {
+                    data.url = path;
+                    load.save(data,
+                        {success: function(id) {
+                            router.navigate('/image/'+id.id, true);
+                        }
+                    });
+                }
+            });
+        },
+        url: '/store',
+        processData: false,
+        contentType: false
+    });
 
 
     var UsersView = Backbone.View.extend({
@@ -36,14 +88,75 @@
                 this.$el.html('hiya!');
                 return;
             }
-            for (var html = '', i = 0; i < images.length; i++) {
-                html += '<div>' + images[i].title + ',' + images[i].description + '</div>';
+            var html = '<div class="flexContainer">';
+            for (var i = 0; i < images.length; i++) {
+                html += '<div class="imageContainer">' + '<a href="/#image/'+images[i].id+'"><img class="image" id="'+images[i].id+'" src="' + images[i].url + '" />'+ '<div class="info"><h3 class="title">'+ images[i].title+ '</h3><br /><p class="description">' + images[i].description + '</p></div></a></div>';
             }
+            html += '</div>';
             this.$el.html(html);
+        },
+        boxMouseOver: function(e) {
+            var target = e.target.nextSibling;
+            $(target).css('z-index', '5');
+        },
+        boxMouseOut: function(e) {
+            var target = e.target.nextSibling;
+            $(target).css('z-index', '-5');
+        },
+        events: {
+            'mouseenter .imageContainer': 'boxMouseOver',
+            'mouseleave .imageContainer': 'boxMouseOut',
         }
-
-
     });
+
+    var ImageView = Backbone.View.extend({
+        initialize: function() {
+            this.render();
+            var view = this;
+            this.model.on('change', function() {
+                view.render();
+            });
+        },
+        render: function() {
+            var image = this.model.attributes.rows;
+            if (!image) {
+                this.$el.html('Loading!');
+                return;
+            }
+            var html = '<div class="singleImageFlex"><div class="singleImageContainer"><img class="singleImage" src="'+image[0].url+'" /><br /><h2 class="singleTitle">'+image[0].title+'</h2><p class="singleDescription">'+image[0].description+'</p></div></div>';
+            this.$el.html(html);
+        },
+    });
+
+    var UploadView = Backbone.View.extend({
+        initialize: function() {
+            this.render();
+            var view = this;
+            this.model.on('change', function(){
+                view.render();
+            });
+        },
+        render : function(){
+            var html = '<form class="upload"><input type="text" placeholder="title" id="titleInput"><input type="text" placeholder="description" id="descriptionInput"><input type="file"><input type="button" name="submit" value="Submit" id="submit"></form>';
+            this.$el.html(html);
+        },
+        events: {
+            'click #submit': 'upload'
+        },
+        upload: function(){
+            var file = $('input[type="file"]').get(0).files[0];
+            var title = $('#titleInput').val();
+            var description = $('#descriptionInput').val();
+            var formData = new FormData();
+            formData.append('file', file);
+            this.model.upload({
+                file: formData,
+                title: title,
+                description: description
+            });
+        }
+    });
+
     var router = new Router();
     Backbone.history.start();
 })();
